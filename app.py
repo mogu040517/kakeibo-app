ff4ee01a12407a57dde2777328347e60b3c03293
mogu040517 <mogumogu2004517@gmail.com>
2025-05-31 13:42:43 +0900

    エラーを出す

--- app.py
+++ app.py
@@ -12,13 +12,18 @@ app.secret_key = os.getenv('SECRET_KEY')
 
 # === DB接続関数 ===
 def get_db_connection():
-    return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-    )
+    try:
+        return mysql.connector.connect(
+            host=os.getenv("DB_HOST"),
+            port=int(os.getenv("DB_PORT")),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME")
+        )
+    except Exception as e:
+        print("DB接続失敗:", e)
+        return None
+
 def init_db():
     conn = get_db_connection()
     cursor = conn.cursor()
